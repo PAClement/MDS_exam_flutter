fe3d29ca88ac3df086cc53a241d6a10dbfca2722
PAClement <clementpaquentin@hotmail.fr>
2023-02-17 12:38:02 +0100

    add flutter map on project

--- lib/ui/screens/trame/trame_map.dart
+++ lib/ui/screens/trame/trame_map.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class TrameMap extends StatefulWidget {
   const TrameMap({Key? key}) : super(key: key);
@@ -13,9 +15,15 @@ class _TrameMapState extends State<TrameMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: Center(
         child: Column(
-          children: [Text("MAP")],
+          children: [
+            Flexible(
+              child: FlutterMap(
+                options: MapOptions(center: LatLng(23.7, 90.3), zoom: 8),
+              ),
+            )
+          ],
         ),
       ),
     );
