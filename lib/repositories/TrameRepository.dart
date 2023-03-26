528fc134bf17fd7d19a2fa77f4364d0614d64438
clement <clementpaquentin@hotmail.fr>
2023-03-26 03:22:59 +0200

    WIP - add Trame Repo

---
+++ lib/repositories/TrameRepository.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import '../models/model_trame.dart';
+
+class TrameRepository{
+  Future<List<ModelTrame>> fetchStopTrame(String query) async{
+
+    final Response response = await get(
+      Uri.parse('https://data.angers.fr/api/records/1.0/search/?'
+          'dataset=horaires-theoriques-et-arrets-du-reseau-irigo-gtfs'
+          '&q=&facet=stop_name&facet=wheelchair_boarding&facet=stop_id'
+          '&refine.stop_name=$query')
+    );
+
+
+      final List<ModelTrame> stopTrames = [];
+
+      final Map<String, dynamic> json = jsonDecode(response.body);
+      print(jsonDecode(response.body));
+      if(json.containsKey("records")){
+        final List<dynamic> records = json['records'];
+
+        for (Map<String, dynamic> record in records){
+          final ModelTrame stopTrame = ModelTrame.fromGeoJson(record);
+
+
+          stopTrames.add(stopTrame);
+        }
+      }
+
+      return stopTrames;
+
+
+  }
+}
